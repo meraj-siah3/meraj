21575a88168310501136603d31573919f3026255
meraj-siah3 <siahtirimeraj@gmail.com>
2025-07-26 13:39:19 -0700

    b

---
+++ ManagementService/Controllers/ModuleController .cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SharedModels.Models;
+
+
+namespace MessageManager.Controllers;
+
+[ApiController]
+[Route("api/module")]
+public class ModuleController : ControllerBase
+{
+    private static readonly Random _rand = new();
+
+    [HttpPost("health")]    
+    public IActionResult PostHealth([FromBody] HealthCheckRequest request)
+    {
+        var response = new HealthCheckResponse
+        {
+            IsEnabled = true,
+            NumberOfActiveClients = _rand.Next(0, 6),
+            ExpirationTime = DateTime.UtcNow.AddMinutes(10).ToString("o")
+        };
+
+        Console.WriteLine($"📥 HealthCheck از توزیع‌گر دریافت شد. Id={request.Id}");
+
+        return Ok(response);
+    }
+}
